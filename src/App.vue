4f406a9fb20f551fb4a79ea1f64191f4bbece499
CPU_Master <adil.smaili@gmail.com>
2017-07-29 16:26:05 +0100

    third

--- src/App.vue
+++ src/App.vue
@@ -6,26 +6,30 @@
       <q-drawer ref="drawer" swipe-only>
         <div class="list platform-delimiter">
           <div class="list-header">
-            Left Side Drawer
+            <h4>Menu</h4>
           </div>
-          <q-drawer-link icon="mail" to="/">Account</q-drawer-link>
-          <q-drawer-link icon="alarm" to="/articles">Alarms</q-drawer-link>
+          <q-drawer-link icon="home" to="/">Home</q-drawer-link>
+          <q-drawer-link icon="list" to="/articles">Articles</q-drawer-link>
         </div>
       </q-drawer>
   
-      <div slot="header" class="toolbar">
+      <div slot="header" class="toolbar amber">
         <!-- opens drawer using its ref -->
         <button class="hide-on-drawer-visible" @click="$refs.drawer.open()">
           <i>menu</i>
         </button>
         <q-toolbar-title :padding="2">
-          Title
+          Galerie Talsint
         </q-toolbar-title>
-        <router-link icon="home" to="/" tag="button">Home</router-link>
-        <router-link icon="list" to="/articles" tag="button">Articles</router-link>
+        <div class="nav-links">  
+          <router-link to="/" tag="button">
+            <i>home</i> Home</router-link>
+          <router-link to="/articles" tag="button">
+            <i>list</i> Articles</router-link>
+        </div>
       </div>
   
-    <router-view></router-view>
+      <router-view></router-view>
     </q-layout>
   </div>
 </template>
@@ -37,4 +41,18 @@
 export default {}
 </script>
 
-<style></style>
+<style lang="stylus">
+.toolbar.amber
+  color #333
+.list-header
+  h4
+    text-align center
+.drawer-content
+  background #ffc107
+.list
+  border 0
+@media screen and (max-width: 920px)
+  .nav-links
+    display none
+
+</style>
